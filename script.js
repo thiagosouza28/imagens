3faa3fbaccc60889f063e438b0945c7b7a143240
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2024-04-18 19:15:00 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 
+<script>
 const imageDirectory = 'https://moura-grafica.vercel.app/img/'; // Diretório onde estão as imagens
 const galleryElement = document.getElementById('gallery');
 
 // Função para carregar e exibir as imagens do diretório
 function loadImages() {
     fetch(imageDirectory)
-        .then(response => response.text())
-        .then(text => {
-            const parser = new DOMParser();
-            const htmlDoc = parser.parseFromString(text, 'text/html');
-            const images = htmlDoc.getElementsByTagName('a');
-
-            for (let image of images) {
-                if (image.href.endsWith('.png') || image.href.endsWith('.jpg')) {
-                    const imgElement = document.createElement('img');
-                    imgElement.src = imageDirectory + image.getAttribute('href');
-                    imgElement.style.maxWidth = '100%';
-                    galleryElement.appendChild(imgElement);
-                }
-            }
+        .then(response => response.blob())
+        .then(blob => {
+            const imageUrl = URL.createObjectURL(blob);
+            const imgElement = document.createElement('img');
+            imgElement.src = imageUrl;
+            imgElement.style.maxWidth = '100%';
+            galleryElement.appendChild(imgElement);
         });
 }
 
