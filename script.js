eca30783eb77058a58ac8e8b4c71cba16683e4c0
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2024-04-18 17:51:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-window.onload = function() {
-  const imagensContainer = document.getElementById('imagens');
+const imagens = [];
+let index = 0;
+const intervalo = 2000; // 2 segundos
 
-  fetch('https://moura-grafica.vercel.app/img')
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Erro ao carregar as imagens');
-    })
-    .then(data => {
-      data.forEach(imagemUrl => {
-        const img = document.createElement('img');
-        img.src = imagemUrl;
-        img.style.marginRight = '10px';
-        imagensContainer.appendChild(img);
-      });
-    })
-    .catch(error => {
-      console.error(error);
-    });
-};
+// Carrega a lista de imagens do diretório
+fetch('https://moura-grafica.vercel.app/img')
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Erro ao carregar as imagens');
+  })
+  .then(data => {
+    imagens.push(...data);
+    exibirProximaImagem();
+    setInterval(exibirProximaImagem, intervalo);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+function exibirProximaImagem() {
+  const imagemElement = document.getElementById('imagem');
+  imagemElement.src = imagens[index];
+  index = (index + 1) % imagens.length;
+}
